792300ffe3081931f8dd619a6d66bf6a68ed7672
Mr.Max` <maximminaev7@gmail.com>
2023-04-23 19:29:43 +0300

    добавили комментарий

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine ("Введите число, чтобы узнать чётность");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0) {
+    Console.Write(number);
+    Console.Write(" - является чётным числом");
+}
+else {
+    Console.Write(number);
+    Console.Write(" - является нечётным числом");
+}
