1dc253529980c4b63dde90645c5ea4e01a5a6c6f
Mr.Max` <maximminaev7@gmail.com>
2023-04-23 19:33:12 +0300

    находим число от 1 до N

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count <= number) {
+    Console.WriteLine (count);
+    count ++;
+}
+
