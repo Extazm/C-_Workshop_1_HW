d4dbfea81a9d911a2f969b832045966f15acde86
Mr.Max` <maximminaev7@gmail.com>
2023-04-23 19:34:42 +0300

    находим наибольшее из двух чисел

--- Task_Master/Program.cs
+++ Task_Master/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b) {
+    Console.Write("Наибольшее число - ");
+    Console.Write(a);
+
+}    
+else if (a < b) {
+    Console.Write("Наибольшее число - ");
+    Console.Write(b);
+}
+else {
+    Console.WriteLine("числа равны");
+}
+//Вот и всё
+
