8826f8bf9c615086e25f113cb288810f15e545fe
Mr.Max` <maximminaev7@gmail.com>
2023-04-23 20:00:28 +0300

    забыл добавить вывод только чётных чисел в диапазоне. добавил

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -2,7 +2,9 @@
 int number = Convert.ToInt32(Console.ReadLine());
 int count = 1;
 while (count <= number) {
-    Console.WriteLine (count);
+    if (count % 2 == 0){
+        Console.WriteLine (count);
+    }
     count ++;
 }
 
